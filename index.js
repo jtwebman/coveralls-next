3f8e832443a25dd0eaef0f107672aa4d4e7ca5cb
JT Turner <jtwebman@gmail.com>
2025-11-08 12:26:17 -0800

    Make code functional to be easier to test.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 const { parseArgs } = require('node:util');
 
-// this needs to go before the other require()s so that
-// the other files can already use index.options
+const convertLcovToCoveralls = require('./lib/convertLcovToCoveralls');
+const sendToCoveralls = require('./lib/sendToCoveralls');
+const { getBaseOptions, getOptions: getOptionsInternal } = require('./lib/getOptions');
+const handleInput = require('./lib/handleInput');
+const logger = require('./lib/logger');
+
+// Parse CLI arguments
 const { values, positionals } = parseArgs({
   options: {
     verbose: { type: 'boolean', short: 'v' },
@@ -13,15 +18,23 @@ const { values, positionals } = parseArgs({
   args: process.argv.slice(2),
 });
 
-// Maintain compatibility with minimist API
-module.exports.options = {
+const cliOptions = {
   ...values,
   _: positionals,
 };
 
-module.exports.convertLcovToCoveralls = require('./lib/convertLcovToCoveralls');
-module.exports.sendToCoveralls = require('./lib/sendToCoveralls');
-module.exports.getBaseOptions = require('./lib/getOptions').getBaseOptions;
-module.exports.getOptions = require('./lib/getOptions').getOptions;
-module.exports.handleInput = require('./lib/handleInput');
-module.exports.logger = require('./lib/logger');
+// Wrapper for getOptions that includes CLI options
+const getOptions = async userOptions => {
+  return getOptionsInternal(userOptions, cliOptions);
+};
+
+module.exports = {
+  convertLcovToCoveralls,
+  sendToCoveralls: obj => sendToCoveralls(obj, cliOptions),
+  getBaseOptions,
+  getOptions,
+  handleInput: (input, userOptions) => handleInput(input, userOptions, cliOptions),
+  logger,
+  // Expose CLI options for backward compatibility with tests
+  options: cliOptions,
+};
--- test/handleInput.js
+++ test/handleInput.js
@@ -3,46 +3,98 @@
 const fs = require('fs');
 const sysPath = require('path');
 require('should');
-const sinon = require('sinon');
-const index = require('..');
+const handleInput = require('../lib/handleInput');
+
+// Mock logger that does nothing
+const mockLogger = {
+  debug: () => {},
+  info: () => {},
+  warn: () => {},
+  error: () => {},
+};
 
 describe('handleInput', () => {
-  afterEach(() => {
-    sinon.restore();
-  });
   it('returns an error when there\'s an error getting options', async () => {
-    sinon.stub(index, 'getOptions').rejects(new Error('some error'));
     const path = sysPath.join(__dirname, './fixtures/onefile.lcov');
     const input = fs.readFileSync(path, 'utf8');
-    await index.handleInput(input).should.be.rejectedWith('some error');
+
+    const dependencies = {
+      getOptions: () => Promise.reject(new Error('some error')),
+      createLogger: () => mockLogger,
+    };
+
+    await handleInput(input, {}, {}, dependencies).should.be.rejectedWith('some error');
   });
+
   it('returns an error when there\'s an error converting', async () => {
-    sinon.stub(index, 'getOptions').resolves({});
-    sinon.stub(index, 'convertLcovToCoveralls').rejects(new Error('some error'));
     const path = sysPath.join(__dirname, './fixtures/onefile.lcov');
     const input = fs.readFileSync(path, 'utf8');
-    await index.handleInput(input).should.be.rejectedWith('some error');
+
+    const dependencies = {
+      getOptions: () => Promise.resolve({}),
+      convertLcovToCoveralls: () => Promise.reject(new Error('some error')),
+      createLogger: () => mockLogger,
+    };
+
+    await handleInput(input, {}, {}, dependencies).should.be.rejectedWith('some error');
   });
+
   it('returns an error when there\'s an error sending', async () => {
-    sinon.stub(index, 'getOptions').resolves({});
-    sinon.stub(index, 'sendToCoveralls').rejects(new Error('some error'));
     const path = sysPath.join(__dirname, './fixtures/onefile.lcov');
     const input = fs.readFileSync(path, 'utf8');
-    await index.handleInput(input).should.be.rejectedWith('some error');
+
+    const dependencies = {
+      getOptions: () => Promise.resolve({}),
+      convertLcovToCoveralls: () => Promise.resolve({}),
+      sendToCoveralls: () => Promise.reject(new Error('some error')),
+      createLogger: () => mockLogger,
+    };
+
+    await handleInput(input, {}, {}, dependencies).should.be.rejectedWith('some error');
   });
+
   it('returns an error when there\'s a bad status code', async () => {
-    sinon.stub(index, 'getOptions').resolves({});
-    sinon.stub(index, 'sendToCoveralls').resolves({ statusCode: 500, body: 'body' });
     const path = sysPath.join(__dirname, './fixtures/onefile.lcov');
     const input = fs.readFileSync(path, 'utf8');
-    await index.handleInput(input).should.be.rejectedWith('Bad response: 500 body');
+
+    const dependencies = {
+      getOptions: () => Promise.resolve({}),
+      convertLcovToCoveralls: () => Promise.resolve({}),
+      sendToCoveralls: () => Promise.resolve({ statusCode: 500, body: 'body' }),
+      createLogger: () => mockLogger,
+    };
+
+    await handleInput(input, {}, {}, dependencies).should.be.rejectedWith('Bad response: 500 body');
   });
+
   it('completes successfully when there are no errors', async () => {
-    sinon.stub(index, 'getOptions').resolves({});
-    sinon.stub(index, 'sendToCoveralls').resolves({ statusCode: 200, body: 'body' });
     const path = sysPath.join(__dirname, './fixtures/onefile.lcov');
     const input = fs.readFileSync(path, 'utf8');
-    const body = await index.handleInput(input);
+
+    const dependencies = {
+      getOptions: () => Promise.resolve({}),
+      convertLcovToCoveralls: () => Promise.resolve({}),
+      sendToCoveralls: () => Promise.resolve({ statusCode: 200, body: 'body' }),
+      createLogger: () => mockLogger,
+    };
+
+    const body = await handleInput(input, {}, {}, dependencies);
     body.should.equal('body');
+  });
+
+  it('uses default parameters when called directly', async () => {
+    const path = sysPath.join(__dirname, './fixtures/onefile.lcov');
+    const input = fs.readFileSync(path, 'utf8');
+
+    // Call handleInput directly (not through index wrapper) to test default parameters
+    // This covers the cliOptions = {} and dependencies = {} default parameters
+    try {
+      const body = await handleInput(input);
+      // If it succeeds, body should be a string
+      body.should.be.type('string');
+    } catch (error) {
+      // If it fails, it should be a "Bad response" error from Coveralls
+      error.message.should.match(/Bad response: 422/);
+    }
   });
 });
